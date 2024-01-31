5a32ae5330d43fb12e7529ecd9fcb259ec6d733f
tiwari-swapnil <swapniltiwari124@gmail.com>
2024-01-31 19:34:49 +0530

    completed projects

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var tl = gsap.timeline();
 tl.to(".yellow-div1",{
     y:"-100%",
     duration:.7,
-    delay:.5,
+    delay:4,
     ease:"expo.out"
 })
 tl.from(".yellow-div2",{
@@ -21,3 +21,40 @@ tl.to(".loader",{
     opacity:0,
     display:"none"
 })
+
+
+
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('.main'),
+    smooth: true
+});
+
+
+let elem = document.querySelectorAll(".elem");
+let page2 = document.querySelector(".page2");
+
+elem.forEach((e)=>{
+    e.addEventListener("mouseenter",()=>{
+        let img = e.getAttribute("data-img");
+        page2.style.background = `url(${img})`
+    })
+})
+
+document.querySelector(".upper-footer h1").addEventListener("click",()=>{
+    scroll.scrollTo(0);
+})
+
+document.querySelector(".ri-arrow-down-s-line").addEventListener("click",()=>{
+    scroll.scrollTo(".page2");
+})
+
+window.onload = function(){
+    let navright = document.querySelector(".nav-right");
+    let btn = document.querySelector(".clickme");
+
+    btn.addEventListener("click",()=>{
+        navright.classList.toggle("open");
+        btn.classList.toggle("rotate")
+    })
+}
+
